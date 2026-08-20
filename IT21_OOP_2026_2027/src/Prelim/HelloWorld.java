6e9e24623ab8027c077ebfb12a5b2dbe4e842cf5
Marchilyn Abunda <marchilynabunda@gmail.com>
2026-08-20 12:45:01 +0800

    Modified Hello.java

--- IT21_OOP_2026_2027/src/Prelim/HelloWorld.java
+++ IT21_OOP_2026_2027/src/Prelim/HelloWorld.java
@@ -3,7 +3,7 @@ package Prelim;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" + "Welcome to IT21A");
 
     }
 
